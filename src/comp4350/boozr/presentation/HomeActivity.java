861cda0d91f90f701538cd08a4cbb4c4c502d831
dexterpeters <dexter11235@me.com>
2016-03-15 19:09:11 -0500

    Removed unused class

--- src/comp4350/boozr/presentation/HomeActivity.java
+++ src/comp4350/boozr/presentation/HomeActivity.java
@@ -59,24 +59,6 @@ public class HomeActivity extends Activity
 
         Main.shutDown();
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) 
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) 
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return super.onOptionsItemSelected(item);
-    }
     
     private void copyDatabaseToDevice() {
     	final String DB_PATH = "db";
